651ae56569eb575c8df2be6f69c8935a8ba6b80a
Ankit Premrajka <ankitp@codeaurora.org>
2012-08-13 08:29:01 -0700

    msm: debug_patch: CLOSE SERVER HANG
    Change-Id: Iec1f8aabbc97b4f9ad53e58f1189fd8dab79559a
    Signed-off-by: Ankit Premrajka <ankitp@codeaurora.org>

--- drivers/media/video/msm/msm.c
+++ drivers/media/video/msm/msm.c
@@ -181,7 +181,7 @@ retry:
 #endif
 	rc = wait_event_interruptible_timeout(queue->wait,
 		!list_empty_careful(&queue->list),
-		out->timeout_ms);
+		msecs_to_jiffies(out->timeout_ms));
 #if defined(CONFIG_SEMC_CAM_MAIN_V4L2) || defined(CONFIG_SEMC_CAM_SUB_V4L2)
 	if (rc == -ERESTARTSYS && cnt < 10) {
 		pr_err("ERESTARTSYS happen cnt=%d\n", cnt);
@@ -253,7 +253,7 @@ static int msm_send_close_server(int vnode_id)
 {
 	int rc = 0;
 	struct msm_ctrl_cmd ctrlcmd;
-	D("%s\n", __func__);
+	pr_err("%s\n", __func__);
 	ctrlcmd.type	   = MSM_V4L2_CLOSE;
 	ctrlcmd.timeout_ms = 10000;
 	ctrlcmd.length	 = strnlen(g_server_dev.config_info.config_dev_name[0],
@@ -2127,7 +2127,7 @@ static long msm_v4l2_evt_notify(struct msm_cam_media_controller *mctl,
 		ERR_COPY_FROM_USER();
 		return -EFAULT;
 	}
-
+	pr_err("%s: Sending event to HAL with type %x\n", __func__, v4l2_ev.type);
 	pcam = mctl->sync.pcam_sync;
 	ktime_get_ts(&v4l2_ev.timestamp);
 	v4l2_event_queue(pcam->pvdev, &v4l2_ev);
