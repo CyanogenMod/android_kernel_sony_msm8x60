e170d42f2441e06e0e671a232bda2ccc97030e14
Ken Zhang <kenz@codeaurora.org>
2012-08-23 19:32:52 -0700

    msm: display: fb blend setting via MSMFB_METADATA_SET ioctl
    MSMFB_METADATA_SET ioctl is added for the fb user to pass
    in additional infomation rather than using reserved
    fields.
    User can set pre-multiplied alpha for the frame buffer via this
    ioctl, overlay blend setting will take this into account.
    
    Change-Id: Ic2f6236d84691fb03174ec479874c6928489ea30
    Signed-off-by: Ken Zhang <kenz@codeaurora.org>
    
    Conflicts:
    	include/linux/msm_mdp.h

--- include/linux/msm_mdp.h
+++ include/linux/msm_mdp.h
@@ -483,7 +483,23 @@ struct msmfb_mdp_pp {
 	} data;
 };
 
+enum {
+	metadata_op_none,
+	metadata_op_base_blend,
+	metadata_op_max
+};
 
+struct mdp_blend_cfg {
+	uint32_t is_premultiplied;
+};
+
+struct msmfb_metadata {
+	uint32_t op;
+	uint32_t flags;
+	union {
+		struct mdp_blend_cfg blend_cfg;
+	} data;
+};
 struct mdp_page_protection {
 	uint32_t page_protection;
 };
