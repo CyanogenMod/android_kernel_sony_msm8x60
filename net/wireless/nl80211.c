4672a9c484d9f5a08a9d8d7b92a5337ca4b2e5c5
Giulio Cervera <giulio.cervera@gmail.com>
2012-10-06 13:31:29 +0200

    Revert "cfg80211: Remove strict validation of AKM suites"
    This reverts commit fd4c84a49e61c508c0e6bfe9dd4407241e7d545a.

--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -3953,6 +3953,12 @@ static bool nl80211_valid_wpa_versions(u32 wpa_versions)
 #endif
 }
 
+static bool nl80211_valid_akm_suite(u32 akm)
+{
+	return akm == WLAN_AKM_SUITE_8021X ||
+		akm == WLAN_AKM_SUITE_PSK;
+}
+
 static bool nl80211_valid_cipher_suite(u32 cipher)
 {
 	return cipher == WLAN_CIPHER_SUITE_WEP40 ||
@@ -4117,7 +4123,7 @@ static int nl80211_crypto_settings(struct cfg80211_registered_device *rdev,
 
 	if (info->attrs[NL80211_ATTR_AKM_SUITES]) {
 		void *data;
-		int len;
+		int len, i;
 
 		data = nla_data(info->attrs[NL80211_ATTR_AKM_SUITES]);
 		len = nla_len(info->attrs[NL80211_ATTR_AKM_SUITES]);
@@ -4130,6 +4136,10 @@ static int nl80211_crypto_settings(struct cfg80211_registered_device *rdev,
 			return -EINVAL;
 
 		memcpy(settings->akm_suites, data, len);
+
+		for (i = 0; i < settings->n_akm_suites; i++)
+			if (!nl80211_valid_akm_suite(settings->akm_suites[i]))
+				return -EINVAL;
 	}
 
 	return 0;
