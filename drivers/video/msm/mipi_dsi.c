2641d55821205f61798ba20dcf340b93d0acd9fe
Neti Ravi Kumar <ravineti@codeaurora.org>
2012-11-09 17:49:47 +0530

    Revert "msm_fb: display: mdp clcoks control done by driver"
    This reverts commit b07513d6e13aec1823fdad69597a93855f69ea40.
    
    Conflicts:
    
    	drivers/video/msm/mipi_dsi_host.c
    
    Change-Id: I2316ce10e2d89758a0f9e7d5a36a032ee0bcf3b7
    Signed-off-by: Neti Ravi Kumar <ravineti@codeaurora.org>

--- drivers/video/msm/mipi_dsi.c
+++ drivers/video/msm/mipi_dsi.c
@@ -79,14 +79,13 @@ static int mipi_dsi_off(struct platform_device *pdev)
 
 	mdp4_overlay_dsi_state_set(ST_DSI_SUSPEND);
 
-	if (mfd->panel_info.type == MIPI_CMD_PANEL) {
-		mipi_dsi_prepare_clocks();
-		mipi_dsi_ahb_ctrl(1);
-		mipi_dsi_clk_enable();
+	/* make sure dsi clk is on so that
+	 * dcs commands can be sent
+	 */
+	mipi_dsi_clk_cfg(1);
 
-		/* make sure dsi_cmd_mdp is idle */
-		mipi_dsi_cmd_mdp_busy();
-	}
+	/* make sure dsi_cmd_mdp is idle */
+	mipi_dsi_cmd_mdp_busy();
 
 	/*
 	 * Desctiption: change to DSI_CMD_MODE since it needed to
@@ -308,11 +307,7 @@ static int mipi_dsi_on(struct platform_device *pdev)
 			}
 			mipi_dsi_set_tear_on(mfd);
 		}
-		mipi_dsi_clk_disable();
-		mipi_dsi_ahb_ctrl(0);
-		mipi_dsi_unprepare_clocks();
 	}
-
 
 #ifdef CONFIG_MSM_BUS_SCALING
 	mdp_bus_scale_update_request(2);
