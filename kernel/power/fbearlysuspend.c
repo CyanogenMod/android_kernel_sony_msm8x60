c3318298d721187d71fc02e8b7a94249e28b7c61
kushdeck <kushdeck@gmail.com>
2012-11-10 04:09:02 -0800

    power: fbearlysuspend: add sleep hack for CRT-off animation
    HTC panels don't seem to work correctly with the Android
    CRT-off animation. Add a short sleep period to let userspace
    finish drawing the animation.
    
    Change-Id: If9526a911ef585375a441a7f13d1674537eec3ec

--- kernel/power/fbearlysuspend.c
+++ kernel/power/fbearlysuspend.c
@@ -13,6 +13,7 @@
  *
  */
 
+#include <linux/delay.h>
 #include <linux/earlysuspend.h>
 #include <linux/module.h>
 #include <linux/wait.h>
@@ -35,6 +36,12 @@ static void stop_drawing_early_suspend(struct early_suspend *h)
 {
 	int ret;
 	unsigned long irq_flags;
+
+#ifdef CONFIG_ARCH_MSM8960
+	/* FIXME: earlysuspend breaks androids CRT-off animation
+	 * Sleep a little bit to get it played properly */
+	msleep(350);
+#endif
 
 	spin_lock_irqsave(&fb_state_lock, irq_flags);
 	fb_state = FB_STATE_REQUEST_STOP_DRAWING;
