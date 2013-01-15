6c582f7323abbb97a331721e227a3c44c95fd342
Mitchel Humpherys <mitchelh@codeaurora.org>
2013-01-15 15:38:52 -0800

    msm: iommu: re-use existing buffers for `extra' mappings
    There is a bug on some hardware that requires us to overmap Iommu
    mappings by 2x. Currently, we set aside a dummy buffer onto which we
    map all of these dummy mappings. In general, for large mappings it's
    nice to use the more efficient iommu_map_range instead of calling
    iommu_map repeatedly. However, with our current approach in
    msm_iommu_map_extra we can't use iommu_map_range for page_sizes larger
    than the dummy buffer. To avoid wasting memory by increasing the size
    of the dummy buffer, we can simply remap on top of the the buffer
    being mapped in the first place. Since the second mapping should never
    be used (besides by the buggy hardware) this should not be a problem.
    
    Re-use existing buffers for all `extra' mappings. Essentially, map the
    same physical address range twice. To be extra safe, make the second
    mapping read-only.
    
    Change-Id: I35462ad50de8da1f2befa3e3f0895925535cdc98
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>

--- drivers/gpu/ion/ion_system_heap.c
+++ drivers/gpu/ion/ion_system_heap.c
@@ -307,8 +307,9 @@ int ion_system_heap_map_iommu(struct ion_buffer *buffer,
 
 	extra_iova_addr = data->iova_addr + buffer->size;
 	if (extra) {
-		ret = msm_iommu_map_extra(domain, extra_iova_addr, extra, SZ_4K,
-					  prot);
+		unsigned long phys_addr = sg_phys(table->sgl);
+		ret = msm_iommu_map_extra(domain, extra_iova_addr, phys_addr,
+					extra, SZ_4K, prot);
 		if (ret)
 			goto out2;
 	}
@@ -527,8 +528,9 @@ int ion_system_contig_heap_map_iommu(struct ion_buffer *buffer,
 
 	if (extra) {
 		unsigned long extra_iova_addr = data->iova_addr + buffer->size;
-		ret = msm_iommu_map_extra(domain, extra_iova_addr, extra, SZ_4K,
-					  prot);
+		unsigned long phys_addr = sg_phys(sglist);
+		ret = msm_iommu_map_extra(domain, extra_iova_addr, phys_addr,
+					extra, SZ_4K, prot);
 		if (ret)
 			goto out2;
 	}
