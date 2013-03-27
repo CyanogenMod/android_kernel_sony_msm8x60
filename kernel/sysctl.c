f986b4fdfee9e9315c90a1ae88850fc794a73e45
David Keitel <dkeitel@codeaurora.org>
2013-03-26 18:50:03 -0700

    sysctl: add cold_boot sysctl entry
    Add a cold_boot parameter which supplements the
    boot_reason sysctl entry with information about
    whether the system was booted from cold or warm state.
    
    /proc/sys/kernel/cold_boot entry is updated with 1 or 0 when
    system was booted from cold or warm boot state respecitively.
    
    CRs-Fixed: 461256
    Change-Id: I2bc5d80c8f26eb9e9dbb4b34960d991a51a224e4
    Signed-off-by: David Keitel <dkeitel@codeaurora.org>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1016,7 +1016,15 @@ static struct ctl_table kern_table[] = {
 		.maxlen		= sizeof(int),
 		.mode		= 0444,
 		.proc_handler	= proc_dointvec,
-},
+	},
+
+	{
+		.procname	= "cold_boot",
+		.data		= &cold_boot,
+		.maxlen		= sizeof(int),
+		.mode		= 0444,
+		.proc_handler	= proc_dointvec,
+	},
 #endif
 #ifdef CONFIG_ARCH_MMAP_RND_BITS
 	{
