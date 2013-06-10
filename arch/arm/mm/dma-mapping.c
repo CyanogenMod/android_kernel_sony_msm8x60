0d96e926e4065a357e75326c16b169b4f3fbd631
Will Deacon <will.deacon@arm.com>
2013-06-10 19:34:39 +0100

    ARM: dma-mapping: convert DMA direction into IOMMU protection attributes
    IOMMU mappings take a prot parameter, identifying the protection bits
    to enforce on the newly created mapping (READ or WRITE). The ARM
    dma-mapping framework currently just passes 0 as the prot argument,
    resulting in faulting mappings.
    
    This patch infers the protection attributes based on the direction of
    the DMA transfer.
    
    Change-Id: If9c78eca2c9cea2f7309d5e5282127eef92645b2
    Cc: Marek Szyprowski <m.szyprowski@samsung.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Git-commit: 13987d68bcf476a93fa864ea4e58755f1bc4bd30
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -1414,7 +1414,7 @@ static dma_addr_t arm_iommu_map_page(struct device *dev, struct page *page,
 {
 	struct dma_iommu_mapping *mapping = dev->archdata.mapping;
 	dma_addr_t dma_addr;
-	int ret, len = PAGE_ALIGN(size + offset);
+	int ret, prot, len = PAGE_ALIGN(size + offset);
 
 	if (!arch_is_coherent())
 		__dma_page_cpu_to_dev(page, offset, size, dir);
@@ -1423,7 +1423,21 @@ static dma_addr_t arm_iommu_map_page(struct device *dev, struct page *page,
 	if (dma_addr == DMA_ERROR_CODE)
 		return dma_addr;
 
-	ret = iommu_map(mapping->domain, dma_addr, page_to_phys(page), len, 0);
+	switch (dir) {
+	case DMA_BIDIRECTIONAL:
+		prot = IOMMU_READ | IOMMU_WRITE;
+		break;
+	case DMA_TO_DEVICE:
+		prot = IOMMU_READ;
+		break;
+	case DMA_FROM_DEVICE:
+		prot = IOMMU_WRITE;
+		break;
+	default:
+		prot = 0;
+	}
+
+	ret = iommu_map(mapping->domain, dma_addr, page_to_phys(page), len, prot);
 	if (ret < 0)
 		goto fail;
 
