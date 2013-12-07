94bd527f8f8b9d6c2b222e2415f35b9614c8051f
Osvaldo Banuelos <osvaldob@codeaurora.org>
2013-12-06 16:15:15 -0800

    cpufreq: ondemand: fix timer-related list corruption in store_powersave_bias()
    The dbs_timer_init() call in store_powersave_bias() re-initializes the
    dbs_info workqueue, call on dbs_timer_exit() to ensure
    outstanding work is cleared prior to making this call. Also, grab the
    percpu timer_mutex lock to avoid race conditions with respect to the
    dbs timer.
    
    Change-Id: I79f3d43eeb51d2d8e21edd0fe043d6583333951f
    Signed-off-by: Osvaldo Banuelos <osvaldob@codeaurora.org>

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -625,14 +625,17 @@ static ssize_t store_powersave_bias(struct kobject *a, struct attribute *b,
 
 				cpumask_set_cpu(cpu, &cpus_timer_done);
 				if (dbs_info->cur_policy) {
+					dbs_timer_exit(dbs_info);
 					/* restart dbs timer */
 					if (timer_pending(
 						&dbs_info->work.timer))
 						dbs_timer_exit(dbs_info);
 
+					mutex_lock(&dbs_info->timer_mutex);
 					dbs_timer_init(dbs_info);
 					/* Enable frequency synchronization
 					 * of CPUs */
+					mutex_unlock(&dbs_info->timer_mutex);
 					atomic_set(&dbs_info->sync_enabled, 1);
 				}
 skip_this_cpu:
