c6fef130d920ffbd67edc5275fa0aca91b7aa44c
Ed Tam <etam@google.com>
2015-03-11 16:41:38 -0700

    Increase watchdog bark timeout to 45s
    Bug: 18759528
    Change-Id: I43bb37cec22d2a03ff734600bb2c523eda90e77e

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -1991,7 +1991,7 @@ struct platform_device msm_device_smd = {
 
 static struct msm_watchdog_pdata msm_watchdog_pdata = {
 	.pet_time = 10000,
-	.bark_time = 11000,
+	.bark_time = 45000,
 	.has_secure = true,
 	.base = MSM_TMR0_BASE + WDT0_OFFSET,
 };
