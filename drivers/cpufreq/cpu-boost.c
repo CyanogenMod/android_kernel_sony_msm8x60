d474d77bc9139a49af26ada2b7f57cf85f8326d7
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-01-21 18:57:49 +0800

    cpufreq: cpu-boost: Remove policy update for source CPU
    This is unnecessary now as the corresponding functionality
    has long been removed.
    
    Change-Id: If0ada068d656f0d6ff0758f30f7ed06fccba8ca8

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -140,7 +140,7 @@ static int boost_mig_sync_thread(void *data)
 	struct cpufreq_policy src_policy;
 	unsigned long flags;
 
-	while(1) {
+	while (1) {
 		wait_event_interruptible(s->sync_wq, s->pending ||
 					kthread_should_stop());
 
