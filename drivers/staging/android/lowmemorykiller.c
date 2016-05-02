b9bb1269f305800b08943e93c3f65b25accea00e
RonGokhale <cips173@gmail.com>
2016-05-03 01:40:54 +0530

    android/lowmemorykiller: Keep inline with upstream
    Change-Id: I199e42517b03bdc66d44df7610976661b53cb774

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -424,7 +424,7 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	}
 	if (nr_to_scan > 0) {
 		ret = adjust_minadj(&min_score_adj);
-		lowmem_print(3, "lowmem_shrink %lu, %x, ofree %d %d, ma %d\n",
+		lowmem_print(3, "lowmem_shrink %lu, %x, ofree %d %d, ma %hd\n",
 				nr_to_scan, sc->gfp_mask, other_free,
 				other_file, min_score_adj);
 	}
@@ -510,6 +510,7 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 		selected_oom_score_adj = oom_score_adj;
 		lowmem_print(3, "select '%s' (%d), adj %hd, size %d, to kill\n",
 			     p->comm, p->pid, oom_score_adj, tasksize);
+
 	}
 	if (selected) {
 		lowmem_print(1, "Killing '%s' (%d), adj %hd,\n" \
